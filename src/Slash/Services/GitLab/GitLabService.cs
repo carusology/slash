64eaa48e7c394a44986f9b727c0c9cf194067d1e
Brian Caruso <carusobp@gmail.com>
2019-12-29 11:59:26 -0500

    Add missing documentation around GitLab service

--- src/Slash/Services/GitLab/GitLabService.cs
+++ src/Slash/Services/GitLab/GitLabService.cs
@@ -29,6 +29,23 @@ namespace Slash.Services.GitLab {
             this.client.BaseAddress = configuration.Value.BaseUrl;
         }
 
+        /// <summary>
+        ///   Performs a GET HTTP request to the location configured via the
+        ///   <see cref="GitLabConfiguration.BaseUrl" /> property to determine
+        ///   if is reachable.
+        /// </summary>
+        /// <remarks>
+        ///   The response from the request must be a 2XX HTTP status code in order
+        ///   for the ping to be considered "successful."
+        /// </remarks>
+        /// <param name="cancellationToken">
+        ///   Optional. Pass to provide the ability to cancel the asynchronous request
+        ///   after it has been initiated.
+        /// </param>
+        /// <returns>
+        ///   A <see cref="PingResult" /> that contains data on whether or not
+        ///   the ping was considered successful.
+        /// </returns>
         public async Task<PingResult> PingAsync(
             CancellationToken cancellationToken = default(CancellationToken)) {
 
--- src/Slash/Services/GitLab/PingResult.cs
+++ src/Slash/Services/GitLab/PingResult.cs
@@ -5,6 +5,10 @@ namespace Slash.Services.GitLab {
 
     public sealed class PingResult : ImmutableBase<PingResult> {
 
+        /// <summary>
+        ///   Whether or not the request to "ping" the instance of GitLab
+        ///   was successful.
+        /// </summary>
         public bool IsSuccessful { get; }
 
         public PingResult(bool isSuccessful = false) {
